cf1fb70639ce260374d349beb62c821d416b5bdc
Mohammad-Mubarak <beyourbestversion3@gmail.com>
2023-01-27 19:34:52 +0530

    done

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key':`b6d43456b4msh4269427a507ce22p138de2jsn8c3088268dd5`,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
